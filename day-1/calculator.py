3b505993e859efb9b7e80b5693c8f7977d4cb621
shahzaib <i.m.shahzaib.243@gmail.com>
2026-05-09 20:31:34 +0500

    caldulator code modified

--- day-1/calculator.py
+++ day-1/calculator.py
@@ -1,23 +1,17 @@
 def add(a, b):
     return a + b
-
 def subtract(a, b):
     return a - b
-
 def multiply(a, b):
     return a * b
-
 def divide(a, b):
     if b != 0:
         return a / b
     else:
         return "Cannot divide by zero"
-
-# Example usage
 x = float(input("Enter first number: "))
 y = float(input("Enter second number: "))
 operation = input("Choose operation (+, -, *, /): ")
-
 if operation == "+":
     print("Result:", add(x, y))
 elif operation == "-":
